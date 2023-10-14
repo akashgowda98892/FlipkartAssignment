25a98ec131b319a9cf27b3fcda9ca20dae6b47df
akashgowda98892 <136059520+akashgowda98892@users.noreply.github.com>
2023-10-14 09:44:21 +0530

    Create Content.js

---
+++ Content.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Content.css'; // Assuming you create a separate CSS file for the content
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div className="services-offered" />
+      <div className="org-setup" />
+      <div className="org-setup-text" />
+      <div className="details-text" />
+      <div className="page-configure-text" />
+      <div className="upload-image" />
+      <div className="bulk-upload-text" />
+      <div className="save-text" />
+      <div className="image" />
+      <div className="solid-border" />
+      <div className="logo" />
+      <div className="org-id-field" />
+      <div className="field-box" />
+      <div className="org-name-field" />
+      <div className="field-box" />
+      <div className="similar-month-field" />
+      <div className="field-box" />
+      <div className="go-live-month-field" />
+      <div className="field-box" />
+      <div className="integration-month-field" />
+      <div className="field-box" />
+      <div className="ai-cycle-field" />
+      <div className="field-box" />
+      <div className="integration-date-field" />
+      <div className="field-box" />
+      <div className="erp-date-field" />
+      <div className="field-box" />
+      <div className="active-ai-time-field" />
+      <div className="field-box" />
+      <div className="active-ai-qc-time-field" />
+      <div className="field-box" />
+      <div className="lock-setup-text" />
+      <div className="lock-setup-info" />
+      <div className="integration-info-text" />
+      <div className="integration-info" />
+      <div className="ai-info-text" />
+      <div className="ai-info" />
+      <div className="final-image" />
+    </div>
+  );
+};
+
+export default Content;
